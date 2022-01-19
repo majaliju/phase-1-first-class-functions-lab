18af9498468a9968d92f55ad396650d27030a983
majaliju <majalijunyc@gmail.com>
2022-01-19 12:48:12 -0500

    updated but stuck on selectingDrivers

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+
+// the array with drivers (from IndexTest.js)
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(){
+    const firstTwoDrivers = drivers.slice(0, 2);
+    return firstTwoDrivers;
+}
+
+const returnLastTwoDrivers = function(){
+    const lastTwoDrivers = drivers.slice(-2);
+    return lastTwoDrivers;
+}
+
+const selectingDrivers = (returnFirstTwoDrivers, returnLastTwoDrivers);
