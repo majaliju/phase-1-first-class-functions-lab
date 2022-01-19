b926f064a391c225cfff9e0c68c3a4bcdacd5c40
majaliju <majalijunyc@gmail.com>
2022-01-19 15:47:29 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // Code your solution in this file!
 
 // the array with drivers (from IndexTest.js)
-const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
 
-const returnFirstTwoDrivers = function(){
+/*
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']; */
+
+
+const returnFirstTwoDrivers = function(drivers){
     const firstTwoDrivers = drivers.slice(0, 2);
     return firstTwoDrivers;
 }
 
-const returnLastTwoDrivers = function(){
+const returnLastTwoDrivers = function(drivers){
     const lastTwoDrivers = drivers.slice(-2);
     return lastTwoDrivers;
 }
@@ -25,17 +28,16 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-function selectDifferentDrivers(drivers, selectingDrivers){
-    return selectingDrivers;
-}
-
-
-/* more elaborate way to test if selectingDrivers is [0] or [1] 
-function selectDifferentDrivers(drivers, selectingDrivers){
-    if (drivers.slice(0,2) === selectingDrivers[0]){
-        return selectingDrivers[0];
+/*
+function selectDifferentDrivers(drivers, function()){
+    if (testingDrivers = returnFirstTwoDrivers){
+        returnFirstTwoDrivers();
     }
-    else if (drivers.slice(-2)=== selectingDrivers[1]){
-        return selectingDrivers[1];
+    else if (testingDrivers = returnLastTwoDrivers){
+        returnLastTwoDrivers();
     }
 } */
+
+function selectDifferentDrivers(drivers, placeHolderFunction){
+    return placeHolderFunction(drivers);
+}
